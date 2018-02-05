878e18e9589598b57c48fe8fb790f6aa2ba5aa48
Devashish2910 <dgpatel2910@gmail.com>
2018-02-05 04:26:58 -0600

    Add: Infix to Prefix

---
+++ 1.Stacks/Applications/infix-to-prefix/script.js
@@ -0,0 +1,155 @@
+/*
+ Polish Expression: Infix to Prefix
+ N.B - Consider '?' as '/'(for division)
+*/
+class Stack {
+    constructor(capacity = Infinity) {
+        this._capacity = capacity;
+        this._storage = [];
+        this._min = [];
+        this._max = [];
+    }
+    
+    push(el) {
+        if(this._storage.length < this._capacity) {
+            if(this._storage.length === 0) {
+                this._min.push(el);
+                this._max.push(el);
+            } else if (this._storage.length > 0) {
+                if(this._max[this._max.length - 1] < el ) {
+                    this._max.push(el);
+                } else {
+                    this._max.push(this._max[this._max.length - 1])
+                }
+                
+                if(this._min[this._min.length - 1] > el ) {
+                    this._min.push(el);
+                } else {
+                    this._min.push(this._min[this._min.length - 1])
+                }
+            }
+            this._storage.push(el);
+            return this._storage;
+        }
+        return 'Stack is full. Delete some elements.'
+    }
+    
+    pop() {
+        if(this._storage.length > 0) {
+            const top = this._storage[this._storage.length - 1];
+            this._min.pop();
+            this._max.pop();
+            this._storage.pop();
+            return top;
+        }
+        return 'Stack is empty.'
+    }
+    
+    peek() {
+        if(this._storage.length > 0) {
+            const top = this._storage[this._storage.length - 1];
+            return top;
+        }
+        return 'Stack is empty.'
+    }
+    
+    isEmpty() {
+        if(this._storage.length > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    min() {
+        const min = this._min[this._min.length - 1];
+        return min;
+    }
+    
+    max() {
+        const max = this._max[this._max.length - 1];
+        return max;
+    }
+    
+    show() {
+        console.log(this._storage);
+    }
+    size() {
+        return this._storage.length;
+    }
+    
+}
+let stack = new Stack(),
+    prefix = new Stack();
+const precedence = new Map(),
+    operator = '^*?%+-',
+    open = '(',
+    close = ')';
+
+precedence.set('^', 3);
+precedence.set('?', 2);
+precedence.set('*', 2);
+precedence.set('%', 2);
+precedence.set('+', 1);
+precedence.set('-', 1);
+
+foo = str => {
+    str = `(${str})`;
+    let i = str.length - 1;
+    
+    while(i >= 0) {
+        const el = str.charAt(i);       //element on the 'i'th position
+
+        if( el === open || el === close || operator.indexOf(el) >= 0) {
+            if(el === close) {
+                stack.push(el); 
+            } else if( operator.indexOf(el) >= 0 ) {
+                if(stack.size() > 0) {
+                    let isDone = false;
+                    const precedenceEl = precedence.get(el);
+                    
+                    while(!isDone) {
+                        const top = stack.peek(),
+                              precedenceTop = precedence.get(top);
+                        
+                        if(precedenceTop >= precedenceEl) {
+                            prefix.push(stack.pop());
+                            isDone = false;
+                        } else {
+                            stack.push(el);
+                            isDone = true;
+                        }
+                        
+                    }
+                }
+                } else if(el === open) {
+                    let isDone = false;
+                    
+                    while(!isDone) {
+                        const top = stack.peek();
+                        
+                        if(stack.size() > 0) {
+                            if(top === close) {
+                                isDone = true;
+                            } else {
+                                prefix.push(stack.pop());
+                                isDone = false;
+                            }
+                        }
+                    }
+                    stack.pop();
+                }
+            
+        } else {
+            prefix.push(el);
+        }
+        
+        i--;
+    }
+    
+    return prefix._storage.reverse();
+};
+
+
+console.log(foo('A+(B*C-(D?E^F)*)*H'));
+
