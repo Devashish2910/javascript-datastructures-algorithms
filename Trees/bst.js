f6c5c0273d0074576826675f0ffbf4e65c33196b
Devashish2910 <dgpatel2910@gmail.com>
2018-03-09 16:35:32 -0600

    added: tree traversal

--- Trees/bst.js
+++ Trees/bst.js
@@ -7,6 +7,7 @@ class BST {
         this.max = [];  // to track maximum
     }
 
+    // Private: to track min-max value of the tree
     _minmax(key) {
         if (this.min[(this.min.length - 1)] > key) {
             this.min.push(key)
@@ -21,6 +22,7 @@ class BST {
         }
     }
 
+    // Insert Private: supporting method if tree is not empty
     _insert(newNode, root) {
         if (newNode.key < root.key) {   // newNode key is smaller than root
             if(root.left === null) {
@@ -39,6 +41,7 @@ class BST {
         }
     }
 
+    // Insert Element in the tree
     insert(el) {
         const newNode = new Node(el);
         if (this.root == null) { // if tree is empty
@@ -49,11 +52,97 @@ class BST {
              this._insert(newNode, this.root);
         }
     }
+
+    // Has Private: supporting method to find element deep in the tree
+    _has(value, node) {
+        if (node.key > value) {
+            if (node.left === null) {
+                return false;
+            } else {
+                if (node.left.key === value) {
+                    return true;
+                } else {
+                    return this._has(value, node.left);
+                }
+            }
+        } else {
+            if (node.right === null) {
+                return false;
+            } else {
+                if (node.right.key === value) {
+                    return true;
+                } else {
+                    return this._has(value, node.right);
+                }
+            }
+        }
+    }
+
+    // Finds tree has a required element or not (returns bool)
+    has(el) {
+        const current = this.root;
+        if (current.key === el) {  // if root is the required element
+            return true;
+        } else {  // if not
+            return this._has(el, current);
+        }
+    }
+
+    // Private: in-order traverse deep in the tree
+    _inOrderTraverseNode(node, callBack) {
+        if (node) {
+            this._inOrderTraverseNode(node.left, callBack);
+            callBack(node.key);
+            this._inOrderTraverseNode(node.right, callBack);
+        }
+    }
+
+    // In-order traversal (Left - Root - Right)
+    inOrderTraversal(callBack) {
+        const current = this.root;
+        this._inOrderTraverseNode(current, callBack);
+    }
+
+    // Private: supporting pre-order method traverse in tree
+    _preOrderTraverseNode(node, callBack) {
+        if (node) {
+            callBack(node.key);
+            this._preOrderTraverseNode(node.left, callBack);
+            this._preOrderTraverseNode(node.right, callBack);
+        }
+    }
+
+    // Pre-order traversal (Root - Left - Right)
+    preOrderTraversal(callBack) {
+        const current = this.root;
+        this._preOrderTraverseNode(current, callBack);
+    }
+
+    // Private: supporting method for post-order traversal in tree
+    _postOrderTraverseNode(node, callBack) {
+        if (node) {
+            this._postOrderTraverseNode(node.left, callBack);
+            this._postOrderTraverseNode(node.right, callBack);
+            callBack(node.key);
+        }
+    }
+
+    // post-order traversal (Left - Right - Root)
+    postOrderTraversal(callBack) {
+        const current = this.root;
+        this._postOrderTraverseNode(current, callBack);
+    }
 }
 
 const myTree = new BST();
 myTree.insert(10);
 myTree.insert(20);
+myTree.insert(25);
+myTree.insert(27);
 myTree.insert(5);
 myTree.insert(7);
-console.log(myTree)
+
+const sort = (val) => {
+    console.log(val);
+}
+myTree.inOrderTraversal(sort)
